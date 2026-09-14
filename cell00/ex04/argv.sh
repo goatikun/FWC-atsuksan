abc29d0f24e1dc95891ca087cfd2063173bb249c
goatikun <67050616@kmitl.ac.th>
2026-09-15 01:30:18 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ]; then
+if [ $# -eq 0 ]; then
     echo "No arguments supplied"
 else
-    printf '%s\n' "$@"
+    if [ $# -ge 1 ]; then
+        echo "$1"
+    fi
+    if [ $# -ge 2 ]; then
+        echo "$2"
+    fi
+    if [ $# -ge 3 ]; then
+        echo "$3"
+    fi
 fi
